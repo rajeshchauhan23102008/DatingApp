40c3bcb2aa6ded634f6e3d9454646347eba45769
rajesh <rajesh.chauhan23102008@gmail.com>
2018-10-03 00:24:04 +0530

    Jwt Token Auth and User Dtos Added

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,10 +17,50 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            var user = await _repo.Login(userLoginDto.Username.ToLower()
+                                    ,userLoginDto.Password.ToLower());
+
+            if(user != null)
+            {
+                //Generate and Send JWT Token to Client.        
+                var claims = new[] {
+                    new Claim(type: ClaimTypes.NameIdentifier, value:user.Id.ToString()),
+                    new Claim(type:ClaimTypes.Name, value:user.Username)
+                };
+
+                var key = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token")
+                    .Value));
+
+                var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+                var tokenDescriptor = new SecurityTokenDescriptor{
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = DateTime.Now.AddDays(1),
+                    SigningCredentials = creds
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+                return Ok(new {
+                    token = tokenHandler.WriteToken(token)
+                });
+            }
+
+            return Unauthorized();            
         }
 
         [HttpPost("register")]
@@ -40,7 +86,9 @@ namespace DatingApp.API.Controllers
 
             // Send Output.
 
-            return Ok(createdUser);
+            return StatusCode(201);
         }
     }
+
+   
 }
