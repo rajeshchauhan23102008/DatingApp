e2d394c2072186bcfbe15807ef2a5570f8be8965
rajesh <rajesh.chauhan23102008@gmail.com>
2019-08-01 12:46:53 +0530

    one last cor change

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -106,16 +106,16 @@ namespace DatingApp.API
             //     app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             // }
             app.UseAuthentication();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseMvc(
-                routes =>
-                {
-                    routes.MapSpaFallbackRoute(
-                        name: "spa-fallback",
-                        defaults: new { controller = "FallbackSPA", action = "Index" });
-                });
-            // app.UseMvc();
+            // app.UseDefaultFiles();
+            // app.UseStaticFiles();
+            // app.UseMvc(
+            //     routes =>
+            //     {
+            //         routes.MapSpaFallbackRoute(
+            //             name: "spa-fallback",
+            //             defaults: new { controller = "FallbackSPA", action = "Index" });
+            //     });
+            app.UseMvc();
         }
     }
 }
